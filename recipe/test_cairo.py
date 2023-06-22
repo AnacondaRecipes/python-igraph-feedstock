72970a02892c3919219a1de6d3efecb707a83359
Anton Kabanovs <akabanovs@anaconda.com>
2023-06-22 13:55:19 +0100

    Attempt to build on win

--- recipe/test_cairo.py
+++
@@ -1,4 +0,0 @@
-import igraph as ig
-G = ig.Graph.Famous('zachary')
-G['layout'] = G.layout_fruchterman_reingold()
-ig.plot(G, 'test.pdf')
--- recipe/test_graphml.py
+++
@@ -1,4 +0,0 @@
-import igraph as ig
-g = ig.Graph.Tree(127,2)
-g.write_graphml('test.graphml')
-tree = ig.Graph.Read_GraphML('test.graphml')
