585c7dc4aba827a4cf9b3db5d1a5b57b66ab50d4
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2026-03-30 14:03:15 +0200

    use pkg-config on windows too

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,8 @@
 mkdir build
 if errorlevel 1 exit 1
 
-rem use already built igraph from dependency package
-set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+rem use pkg-config to find already built igraph from dependency package
+set IGRAPH_USE_PKG_CONFIG=1
 
 %PYTHON% -m pip install --no-deps --ignore-installed --no-build-isolation . -vvv
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
 #!/bin/env bash
 
+# use pkg-config to find already built igraph from dependency package
 export IGRAPH_USE_PKG_CONFIG=1
 ${PYTHON} -m pip install --no-deps --ignore-installed --no-build-isolation . -vvv
