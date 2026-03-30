e98470b8e9c4ca2bf83d43855169e25c065be0fd
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2026-03-30 13:49:37 +0200

    use prebuilt igraph

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,22 +3,8 @@
 mkdir build
 if errorlevel 1 exit 1
 
-set IGRAPH_CMAKE_EXTRA_ARGS=%CMAKE_ARGS%^
-      -GNinja ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_PREFIX_PATH="%CONDA_PREFIX%" ^
-      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-      -DCMAKE_POSITION_INDEPENDENT_CODE=on ^
-      -DIGRAPH_USE_INTERNAL_BLAS=0 ^
-      -DIGRAPH_USE_INTERNAL_LAPACK=0 ^
-      -DIGRAPH_USE_INTERNAL_ARPACK=1 ^
-      -DIGRAPH_USE_INTERNAL_GLPK=0 ^
-      -DIGRAPH_USE_INTERNAL_GMP=1 ^
-      -DBUILD_SHARED_LIBS=on ^
-      -DIGRAPH_ENABLE_LTO=1 ^
-      -DIGRAPH_ENABLE_TLS=1 ^
-      -DIGRAPH_GRAPHML_SUPPORT=1
-set IGRAPH_EXTRA_LIBRARIES=glpk,libxml2
+rem use already built igraph from dependency package
+set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 
 %PYTHON% -m pip install --no-deps --ignore-installed --no-build-isolation . -vvv
 if errorlevel 1 exit 1
