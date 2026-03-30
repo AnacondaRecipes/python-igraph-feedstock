39a72c304a1e132b460f0606490034799e036dc4
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2026-03-30 14:17:35 +0200

    set pkgconfig path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,7 @@ if errorlevel 1 exit 1
 
 rem use pkg-config to find already built igraph from dependency package
 set IGRAPH_USE_PKG_CONFIG=1
+set PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%PKG_CONFIG_PATH%
 
 %PYTHON% -m pip install --no-deps --ignore-installed --no-build-isolation . -vvv
 if errorlevel 1 exit 1
